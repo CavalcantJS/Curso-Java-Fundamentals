ab40880723f07a1531b37ad116f489ab74eb2960
Jonathan Cavalcante <jonathancavalcante1911@gmail.com>
2025-08-11 16:59:59 -0300

    .

---
+++ bom/pessoas.java
@@ -0,0 +1,26 @@
+package bom;
+
+public class pessoas {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		String pessoa1 = "Jonathan Cavalcante da Silva";
+		String estadocivil1 = "Solteiro";
+		String email1 = "jonathancavalcante1911@gmail.com";
+		
+		String pessoa2 = "Giovane Colacioppo";
+		String estadocivil2 = "Casado";
+		String email2 = "giovaneCC@gmail.com";
+		
+		
+		
+		
+		
+		System.out.println("Nome: " +pessoa1 + ", Estado civil: " + estadocivil1 + ", Email: " + email1);
+		System.out.println("Nome: " +pessoa2 + ", Estado civil: " + estadocivil2 + ", Email: " + email2);
+		
+		
+	}
+
+}
---
+++ pacote1/Calculos.java
@@ -0,0 +1,23 @@
+package pacote1;
+
+public class Calculos {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int X = 200;
+		int Y = 50;
+		int soma = X + Y;
+		
+		System.out.println( X + "+" + Y + "="+ soma);
+		
+		System.out.println( X + "*" + Y + "="+(X * Y));
+		
+		System.out.println( X + "/" + Y + "="+(X / Y));
+		
+		System.out.println( X + "-" + Y + "="+(X - Y));
+		
+		
+	}
+
+}
